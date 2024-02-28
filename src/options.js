784579562398066282162cd7142ef12a118518c7
Jens Schmidt <farblos@vodafonemail.de>
2024-02-28 23:36:15 +0100

    Added copyright headers, updated icons, tweaked manifest.
    * copy-on-select-2.svg: Add SVG and generate ...
    * src/copy-on-select-2-128.png, src/copy-on-select-2-64.png,
    src/copy-on-select-2-32.png: ... these from it to replace ...
    * src/copy-on-select-128.png: ... this one.
    * src/manifest.json: Bump extension version.  Define extension Id
    and minimum browser version.  Define new and differently sized
    icons.  Use recommended "run_at" behavior.

--- src/options.js
+++ src/options.js
@@ -1,3 +1,14 @@
+// options.js - copy-on-select-2 option script.
+//
+// Copyright (C) 2023-2024 Jens Schmidt
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is “Incompatible With Secondary Licenses”, as
+// defined by the Mozilla Public License, v. 2.0.
+
 "use strict";
 
 // polyfill the "browser" global for chromium compatibility
